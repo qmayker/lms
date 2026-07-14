ba844c0705a352193892027b682b3258c1b53cbd
qmayker <qmayker9@gmail.com>
2026-07-14 23:21:37 +0300

    Added models to admin

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from django.contrib.contenttypes.admin import GenericTabularInline
+from .models import Course, Module, Content, TextContent
 
-# Register your models here.
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("name", "author", "created", "edited")
+    list_filter = ("author", "created")
+    search_fields = ("name", "description", "author__username")
+    ordering = ("name",)
+
+
+@admin.register(Module)
+class ModuleAdmin(admin.ModelAdmin):
+    list_display = ("name", "course", "order", "created", "edited")
+    list_filter = ("course", "created")
+    search_fields = ("name", "course__name")
+    ordering = ("course", "order", "name")
+
+    def get_exclude(self, request, obj=None) -> tuple:
+        if obj is None:
+            return ("order",)
+        return ()
+
+
+@admin.register(Content)
+class ContentAdmin(admin.ModelAdmin):
+    list_display = ("module", "content_type", "object_id")
+    list_filter = ("module", "content_type")
+    search_fields = ("module__name", "content_type__model")
+    ordering = ("module", "content_type", "object_id")
+
+
+class ContentInline(GenericTabularInline):
+    model = Content
+    extra = 0
+    max_num = 1
+    min_num = 1
+
+
+@admin.register(TextContent)
+class TextContentAdmin(admin.ModelAdmin):
+    list_display = ("id", "content__module", "text_content")
+    list_filter = ("content__module",)
+    search_fields = ("text_content",)
+    ordering = ("content__module", "id")
+    inlines = [ContentInline]
--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import StudentProfile, TeacherProfile, User
 
 # Register your models here.
 
@@ -9,3 +9,15 @@ from .models import User
 class CustomUserAdmin(UserAdmin):
     fieldsets = UserAdmin.fieldsets + (("additional_info", {"fields": ("role",)}),)
     add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ("role",)}),)
+
+
+@admin.register(StudentProfile)
+class StudentProfileAdmin(admin.ModelAdmin):
+    list_display = ("user",)
+    search_fields = ("user__username", "user__email")
+
+
+@admin.register(TeacherProfile)
+class TeacherProfileAdmin(admin.ModelAdmin):
+    list_display = ("user",)
+    search_fields = ("user__username", "user__email")
